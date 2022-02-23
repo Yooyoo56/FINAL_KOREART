8e562aea23d95d49ec70833a09e14fdac6564210
Yooyoo56 <tak.yoojeong@gmail.com>
2022-02-23 17:30:35 +0100

    gravatar done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,74 +5,52 @@ import "./Navbar.css";
 
 //on peut acceder ->this.props.user
 class Navbar extends React.Component {
-	//	componentDidUpdate(prevProps, prevState){
-	//		if(this.props.user !== prevProps.user){
-	//			this.props.
-	//		}
-	//	}
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props.user !== prevProps.user) {
+			console.log("the user state has changed");
+		}
+	}
 	render() {
-		//		if (this.props.user === false)
-		console.log(this.props.user);
-		return (
-			<nav className="nav-style">
-				<div className="nav">
-					<div className="nav-title">
-						<h1>Kore'Art</h1>
-					</div>
+		if (!this.props.user) {
+			return (
+				<div>
+					<nav className="nav-style">
+						<div className="nav">
+							<div className="nav-title">
+								<h1>Kore'Art</h1>
+							</div>
 
-					<div class="nav-menu">
-						<ul>
-							<li>
-								{this.props.user ? (
-									<Link to="/profile" style={{ textDecoration: "none" }}>
-										Profile
-									</Link>
-								) : (
-									<Link to="/profile" style={{ textDecoration: "none" }}>
-										Profile
-									</Link>
-								)}
-							</li>
-							<li>
-								<Link to="/" style={{ textDecoration: "none" }}>
-									Home
-								</Link>
-							</li>
-							<li>
-								<Link to="/login" style={{ textDecoration: "none" }}>
-									Login
-								</Link>
-							</li>
-							<li>
-								<Link to="/signup" style={{ textDecoration: "none" }}>
-									Signup
-								</Link>
-							</li>
-						</ul>
-					</div>
+							<div class="nav-menu">
+								<ul>
+									<li>
+										{/* profile page -> when refresh the page, doesnt work*/}
+										<Link to="/profile" style={{ textDecoration: "none" }}>
+											Profile
+										</Link>
+									</li>
+									<li>
+										<Link to="/" style={{ textDecoration: "none" }}>
+											Home
+										</Link>
+									</li>
+									<li>
+										<Link to="/login" style={{ textDecoration: "none" }}>
+											Login
+										</Link>
+									</li>
+									<li>
+										<Link to="/signup" style={{ textDecoration: "none" }}>
+											Signup
+										</Link>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</nav>
 				</div>
-			</nav>
-		);
+			);
+		}
 	}
 }
 
 export default Navbar;
-
-/*
-<div class="nav">
-	<div class="nav-title">
-		<h1>Web Zone</h1>
-	</div>
-
-	<div class="nav-menu">
-		<ul>
-			<li>Home</li>
-			<li>Blog</li>
-			<li>Services</li>
-			<li>Demo</li>
-			<li>About</li>
-		</ul>
-	</div>
-</div>
-
-*/
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,66 +1,137 @@
-import React from "react";
 import Popin from "../Popin.js";
 import authService from "./auth-service.js";
-import { Redirect } from "react-router-dom";
+import React, { Component } from "react";
+import "./Profile.css";
 
-class Profile extends React.Component {
+class Profile extends Component {
+	state = {
+		data: [],
+		per: 1,
+		page: 1,
+		total_pages: null,
+	};
+
+	/**protection -> logout */
 	logout = (event) => {
 		authService.logout().then((response) => {
 			this.props.updateUser(false);
 		});
 	};
 
+	/**Uppercase the word */
+	uppercase = (word) => {
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	};
+
+	/**load the data j */
+	loadData = () => {
+		const { per, data } = this.state;
+		const endpoint = `https://randomuser.me/api/?nat=us&results=${per}`;
+		fetch(endpoint)
+			.then((response) => response.json())
+			.then((json) => {
+				this.setState({
+					data: [...data, ...json.results],
+					scrolling: false,
+					total_pages: json.info.results,
+				});
+			});
+	};
+
+	loadMore = () => {
+		this.setState(
+			(prevState) => ({
+				scrolling: true,
+			}),
+			this.loadData
+		);
+	};
+
+	componentDidMount() {
+		this.loadData();
+	}
+
 	render() {
 		if (this.props.user === false)
 			return "You should loggedin to aceess your profile!";
 		else {
-
-			// si user vide, n'affiche rien !!!!!
-			if (!this.props.user._id) return "loading..."
-
-			return (
-				<>
-					<Popin
-						one={
-							<>
-								<h1>Profile</h1>
-								<p>
-									<em>Email:</em>
-									<span>{this.props.user.email}</span>
-								</p>
-								<p>
-									<em>Name:</em>
-									<span>{this.props.user.firstname}</span>
-								</p>
-								<p>
-									<em>City:</em>
-									<span>{this.props.user.city}</span>
-								</p>
-
-								<p>
-								
-									<em>My whishlist:</em>
-
-									{this.props.user.favorites.map(workart => {
-									return (<span>{workart.name}</span>)
-									})
-									}
-								</p>
-
-
-
-								<div className="cta">
-									<button className="btn logout" onClick={this.logout}>
-										logout
-									</button>
-								</div>
-								<div></div>
-							</>
-						}
-					/>
-				</>
-			);
+			if (!this.props.user._id) return "loading...";
 		}
+		return (
+			<div className="clearfix">
+				<div className="row">
+					{this.state.data.map((data) => (
+						<div className="col-md-4 animated fadeIn" key={data.id.value}>
+							<div className="card">
+								<div className="card-body">
+									<div className="avatar">
+										<img
+											src={data.picture.large}
+											className="card-img-top"
+											alt=""
+										/>
+									</div>
+
+									<>
+										<Popin
+											one={
+												<>
+													<h1>Profile</h1>
+
+													<h5 className="card-title">
+														<p>
+															<em>Name:</em>
+															<span>
+																{this.uppercase(this.props.user.firstname)}
+															</span>
+														</p>
+													</h5>
+													<p className="card-text">
+														<em>Email:</em>
+														<span>{this.uppercase(this.props.user.email)}</span>
+													</p>
+
+													<p className="card-text">
+														<em>City:</em>
+														<span>{this.uppercase(this.props.user.city)}</span>
+													</p>
+
+													<p className="card-text">
+														<em>My whishlist:</em>
+
+														{this.props.user.favorites.map((workart) => {
+															return <span>{workart.name}</span>;
+														})}
+													</p>
+
+													<div className="cta">
+														<button
+															className="btn logout"
+															onClick={this.logout}
+														>
+															logout
+														</button>
+													</div>
+													<div></div>
+												</>
+											}
+										/>
+									</>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+				<button
+					className="btn btn-light btn-block w-50 mx-auto"
+					onClick={(e) => {
+						this.loadMore();
+					}}
+				>
+					Load More Users
+				</button>
+			</div>
+		);
 	}
 }
 
