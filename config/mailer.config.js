06126a491e693698386f5824a14181bee0720bf8
Yooyoo56 <tak.yoojeong@gmail.com>
2022-02-26 16:45:54 +0100

    retour..

--- config/mailer.config.js
+++ config/mailer.config.js
@@ -1,9 +1,7 @@
 // config/mailer.config.js
 
 const nodemailer = require("nodemailer");
-const SMTP_URI =
-	"smtps://apikey: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@smtp.sendgrid.net:465/?pool=true";
-const transporter = nodemailer.createTransport(SMTP_URI);
+const transporter = nodemailer.createTransport(process.env.SMTP_URI);
 
 // verify connection configuration
 transporter
